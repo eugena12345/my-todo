b72ec5a9b4b5692f587ba5eca783c882708cac98
eugena12345 <eugena.h12345@gmail.com>
2022-11-17 21:40:33 +0400

    added content Top component

--- src/components/Top.js
+++ src/components/Top.js
@@ -4,7 +4,20 @@ import styles from './Top.module.css';
 const Top = () => {
 return (
     <div className={styles.first}>
-        Верхнее поле приложения
+        <h1>To Do</h1>
+        <input></input>
+        <button>Add Task</button>
+        <div className={styles.select}>
+            <div className={styles.selectDate}>
+                <button>New</button>
+                <button>Old</button>
+            </div>
+            <div className={styles.selectDone}>
+                <button>All</button>
+                <button>Undone</button>
+                <button>Done</button>
+            </div>
+        </div>
     </div>
 )
 }
