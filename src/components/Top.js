4323bbe8c85f32da612babb2076bb27caf2f97fe
eugena12345 <eugena.h12345@gmail.com>
2022-11-18 21:05:10 +0400

    added styles for input Task

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,25 +1,27 @@
 import React from "react";
-import styles from './Top.module.css';
+import styles from "./Top.module.css";
 
 const Top = () => {
-return (
+  return (
     <div className={styles.first}>
-        <h1>To Do</h1>
+      <h1>To Do</h1>
+      <div className={styles.inputTask}>
         <input></input>
         <button>Add Task</button>
-        <div className={styles.select}>
-            <div className={styles.selectDate}>
-                <button>New</button>
-                <button>Old</button>
-            </div>
-            <div className={styles.selectDone}>
-                <button>All</button>
-                <button>Undone</button>
-                <button>Done</button>
-            </div>
+      </div>
+      <div className={styles.select}>
+        <div className={styles.selectDate}>
+          <button>New</button>
+          <button>Old</button>
         </div>
+        <div className={styles.selectDone}>
+          <button>All</button>
+          <button>Undone</button>
+          <button>Done</button>
+        </div>
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default Top;
