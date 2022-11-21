de9ce764877cb85f5b170a144f0d2c3a3d4925eb
eugena12345 <eugena.h12345@gmail.com>
2022-11-21 23:42:25 +0400

    added task change

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,23 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Task.module.css";
 
-const Task = ({taskText, taskId, taskArray, setTaskArray}) => {
-const deleteTask = (taskId) => {
-const newtaskArray = taskArray.filter(item => item.id !== taskId)
-setTaskArray(newtaskArray);
-}
-const editeTask = (taskId) => {
-    console.log('edite task '+ taskId + taskText);
-    }
+const Task = ({ taskText, taskId, taskArray, setTaskArray }) => {
+  const [edit, setEdit] = useState(false);
+  const [value, setValue] = useState("");
+
+  const deleteTask = (taskId) => {
+    const newtaskArray = taskArray.filter((item) => item.id !== taskId);
+    setTaskArray(newtaskArray);
+  };
+  const editeTask = (taskId, taskText) => {
+    setEdit(true);
+    setValue(taskText);
+  };
+
+  const saveChangedTask = (taskId) => {
+    const changedTaskArray = [...taskArray].map((item) => {
+      if (item.id === taskId) {
+        item.taskText = value;
+      }
+      return item;
+    });
+    setTaskArray(changedTaskArray);
+    setEdit(false);
+  };
+
   return (
-    <div className={styles.taskBox}>
-      <input type="checkbox" className={styles.checkbox} />
-      <div className={styles.taskText}>{taskText}</div>
-      <div className={styles.taskButton}>
-        <div>Date</div>
-        <button onClick={()=>editeTask(taskId)}>Edit</button>
-        <button onClick={()=>deleteTask(taskId)}>Delete</button>
-      </div>
+    <div>
+      {edit ? (
+        <div>
+          <input
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          ></input>
+          <button onClick={() => saveChangedTask(taskId)}>Save</button>
+        </div>
+      ) : (
+        <div className={styles.taskBox}>
+          <input type="checkbox" className={styles.checkbox} />
+          <div className={styles.taskText}>{taskText}</div>
+          <div className={styles.taskButton}>
+            <div>Date</div>
+            <button onClick={() => editeTask(taskId, taskText)}>Edit</button>
+            <button onClick={() => deleteTask(taskId)}>Delete</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
